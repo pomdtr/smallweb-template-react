b759a422796d2b423c03e51d32bc421d25f11ccf
pomdtr <achille.lacoin@gmail.com>
2024-06-26 13:27:35 +0200

    update naming

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import { createServer } from "./mod.ts";
+import { createApp } from "./mod.ts";
 
 export default {
-    fetch: createServer({}),
+    fetch: createApp({}),
 };
--- mod.ts
+++ mod.ts
@@ -1,10 +1,11 @@
 import { serveDir } from "./file_server.ts";
 import dir from "./dist/dir.ts";
 
-export type RequestHandler = (req: Request) => Response | Promise<Response>;
-
+// fill this with the types of the parameters you want to pass to your app
 export type AppParams = {};
-export function createServer(_params: AppParams): RequestHandler {
+
+export type RequestHandler = (req: Request) => Response | Promise<Response>;
+export function createApp(_params: AppParams): RequestHandler {
     return (req: Request) => {
         return serveDir(req, {
             dir,
